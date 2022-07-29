9352d9f4515d5ced996028ece7ed03db640ea520
Grant平宿舍 <978066590@qq.com>
2022-07-29 12:13:25 +0800

    0729ghr 1

---
+++ src/main/java/com/package1/package2/Bean/testBean.java
@@ -0,0 +1,21 @@
+package com.package1.package2.Bean;
+
+/**
+ * @ClassName :testBean
+ * @Description :
+ * @Author : 郭浩然
+ * @Date : 2022/07/29/11:38
+ */
+public class testBean {
+
+    /**
+     * MethodName : test1
+     * Description: 
+     * @Params: [a]
+     * @Return: void
+     */
+    void test1(int a){
+        
+    }
+
+}
