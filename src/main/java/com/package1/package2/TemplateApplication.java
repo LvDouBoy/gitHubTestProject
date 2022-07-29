93e5843f710c1458bc6b522df8c484ddd7d63b05
Grant平宿舍 <978066590@qq.com>
2022-07-29 12:16:16 +0800

    0729ghr 3

--- src/main/java/com/package1/package2/TemplateApplication.java
+++ src/main/java/com/package1/package2/TemplateApplication.java
@@ -9,6 +9,7 @@ public class TemplateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TemplateApplication.class, args);
 		System.out.println(1);
+		System.out.println(2);
 	}
 
 }
